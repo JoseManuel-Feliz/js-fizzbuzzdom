8e503f2217599a37c8caa617933ec80f86032362
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-08-09 13:13:56 +0200

    added conditions

--- js/script.js
+++ js/script.js
@@ -4,12 +4,12 @@
 2. Scrivere un programma che stampi in console i numeri da 1 a 100.
     2a. Creare un elemento per contenere i dati creati.
 
-3. Per i multipli di 3 deve stampare “Fizz” al posto del numero.
-
-4. Per i multipli di 5 deve stampare “Buzz” al posto del numero.
-
-5. Per i numeri che sono sia multipli di 3 che di 5 deve stampare 
+3. Per i numeri che sono sia multipli di 3 che di 5 deve stampare 
 “FizzBuzz” al posto del numero.
+    
+4. Per i multipli di 3 deve stampare “Fizz” al posto del numero.
+
+5. Per i multipli di 5 deve stampare “Buzz” al posto del numero.
 
 6. Stampare i dati elaborati all'interno dell'elemento creato e 
 poi inserirlo all'interno dell'elemento d'interesse del DOM.
@@ -30,9 +30,28 @@ let i = ''
 let text = '';
 for (let i = 1; i <= 100; i++) {
     text = i;
-    console.log(text)
 
     //2a. Creare un elemento per contenere i dati creati.
     const p = document.createElement('div');
 
+    /* 3. Per i numeri che sono sia multipli di 3 che di 5 deve stampare 
+    “FizzBuzz” al posto del numero. */
+    if (i % 3 == 0 && i % 5 == 0) {
+        text = 'fizzbuzz'
+    }
+    else {
+
+        //4. Per i multipli di 3 deve stampare “Fizz” al posto del numero.
+        if (i % 3 == 0) {
+            text = 'fizz'
+        }
+
+        //5. Per i multipli di 5 deve stampare “Buzz” al posto del numero.
+        else if
+            (i % 5 == 0) {
+            text = 'buzz'
+        }
+    }
+
+    console.log(text)
 }
