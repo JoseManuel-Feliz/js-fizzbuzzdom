6282799d6ba05bb0d8bb88c8ba6b61eb4f7da284
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2025-01-14 23:17:21 +0100

    feat: project update

--- js/script.js
+++ js/script.js
@@ -34,6 +34,7 @@ const card = document.getElementById('boxes');
 //2. Scrivere un programma che stampi in console i numeri da 1 a 100.
 for (let i = 1; i <= 100; i++) {
     let text = i;
+    console.log(i)
 
     //2a. Creare un elemento per contenere i dati creati.
     const box = document.createElement('div');
